83ec88e82268a39a228011748c582d37b47e2d63
gunwoohan <gunwoohan@naver.com>
2020-12-28 04:17:13 +0900

    Edit : step01.py
    add magic method in variable class
    step 20 complete

--- steps/step01.py
+++ steps/step01.py
@@ -1,43 +1,115 @@
 import numpy as np
+import unittest
+import weakref
+import contextlib
+
+class Config:
+    enable_backprop = True
+
+@contextlib.contextmanager
+def using_config(name,value):
+    old_value = getattr(Config,name)
+    setattr(Config, name, value)
+    try:
+        yield
+    finally:
+        setattr(Config,name,old_value)
+
+def no_grad():
+    return using_config('enable_backprop',False)
 
 class Variable:
-    def __init__(self,data):
+    def __init__(self,data, name=None):
         if data is not None:
             if not isinstance(data, np.ndarray):
                 raise TypeError(f'{type(data)}은(는) 지원하지 않습니다.')
 
-
         self.data = data
+        self.name = name
         self.grad = None
         self.creator= None
+        self.generation = 0
 
     def set_creator(self, func):
         self.creator = func
+        self.generation = func.generation + 1
 
-    def backward(self):
+    def backward(self,retain_grad=False):
         if self.grad is None:
             self.grad = np.ones_like(self.data)
 
-        funcs = [self.creator]
+        funcs = []
+
+        seen_set = set()
+        def add_func(f):
+            if f not in seen_set:
+                funcs.append(f)
+                seen_set.add(f)
+                funcs.sort(key=lambda x: x.generation)
+        
+        add_func(self.creator)
+
         while funcs:
-            f= funcs.pop()
-            x,y = f.input, f.output
-            x.grad = f.backward(y.grad)
+            f = funcs.pop()
+            gys = [output.grad for output in  f.outputs]
+            gxs = f.backward(*gys)
+            if not isinstance(gxs,tuple):
+                gxs = (gxs,)
+            
+            for x, gx in zip(f.inputs,gxs):
+                if x.grad is None:
+                    x.grad = gx
+                else:
+                    x.grad = x.grad + gx
+                
+                if x.creator is not None:
+                    add_func(x.creator)
+        if not retain_grad:
+            for y in f.outputs:
+                y().grad = None
 
-            if x.creator is not None:
-                funcs.append(x.creator)
+    def cleangrad(self):
+        self.grad = None
 
+    @property
+    def shape(self):
+        return self.data.shape
+
+    @property
+    def ndim(self):
+        return self.data.ndim
+
+    @property
+    def size(self):
+        return self.data.size
+    
+    @property
+    def dtype(self):
+        return self.data.dtype
+
+    def __len__(self):
+        return len(self.data) 
+    
+    def __repr__(self):
+        if self.data is None:
+            return 'variable(None)'
+        p = str(self.data).replace('\n','\n' + ' ' * 9)
+        return f'variable({p})'    
 
 class Function:
-    def __call__(self,input):
-        x = input.data
-        y = self.forward(x)
-        output = Variable(as_array(y))
-        output.set_creator(self)
-
-        self.input = input
-        self.output = output
-        return output
+    def __call__(self,*inputs):
+        xs = [x.data for x in inputs]
+        ys = self.forward(*xs)
+        if not isinstance(ys, tuple):
+            ys = (ys,)
+        outputs = [Variable(as_array(y)) for y in ys]   
+        if Config.enable_backprop:
+            self.generation = max([x.generation for x in inputs])
+            for output in outputs:
+                output.set_creator(self)
+            self.inputs = inputs
+            self.outputs = [weakref.ref(output) for output in f.outputs]
+        return outputs if len(outputs) > 1 else outputs[0]
 
     def forward(self,x):
         raise NotImplementedError()
@@ -46,12 +118,14 @@ class Function:
         raise NotImplementedError()
 
 
+
+
 class Square(Function):
     def forward(self,x):
         return x**2
 
     def backward(self,gy):
-        x = self.input.data
+        x = self.inputs[0].data
         gx = 2 * x * gy
         return gx
 
@@ -61,9 +135,18 @@ class Exp(Function):
 
 
     def backward(self,gy):
-        x = self.input.data
+        x = self.inputs
         gx = np.exp(x) * gy
         return gx
+
+class Add(Function):
+    def forward(self,x0,x1):
+        y = x0 + x1
+        return (y,)
+
+    def backward(self,gy):
+        return gy , gy
+
 
 def numerical_diff(f,x,eps=1e-4):
     x0 = Variable(x.data-eps)
@@ -78,12 +161,14 @@ def square(x):
 def exp(x):
     return Exp()(x)
 
+def add(x0,x1):
+    return Add()(x0,x1)
+
 def as_array(x):
     if np.isscalar(x):
         return np.array(x)
     return x
 
-import unittest
 
 class SquareTest(unittest.TestCase):
     def test_forward(self):
@@ -108,4 +193,9 @@ class SquareTest(unittest.TestCase):
         self.assertTrue(flg)
 
 if __name__ == "__main__":
-    unittest.main()
+    # unittest.main()
+    with no_grad():
+        x = Variable(np.ones(10))
+        y = square(x)
+
+        print(x)
